3ae5eff52e62ebc1723aab5c36e5ae0d8c7cef9b
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2023-06-14 22:48:05 +0700

    Sending message

--- utils/waConnections/reconct.js
+++ utils/waConnections/reconct.js
@@ -1,33 +1,53 @@
-// Supports ES6
-// import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
 
 venom
   .create(
-     'session-name', //name of session
-     (base64Qr, asciiQR) => {
-        console.log(asciiQR)
-      },
-      {
-        browserArgs: ['--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'],
-      }
-  ) 
-  .then((client) => start(client))
+    //session
+    'sessionName', //Pass the name of the client you want to start the bot
+    //catchQR
+    (base64Qrimg, asciiQR, attempts, urlCode) => {
+      console.log('Number of attempts to read the qrcode: ', attempts);
+      console.log('Terminal qrcode: ', asciiQR);
+      console.log('base64 image string qrcode: ', base64Qrimg);
+      console.log('urlCode (data-ref): ', urlCode);
+    },
+    // statusFind
+    (statusSession, session) => {
+      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+      //Create session wss return "serverClose" case server for close
+      console.log('Session name: ', session);
+    },
+    // options
+    {
+      folderNameToken: 'tokens', //folder name when saving tokens
+      mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+      headless: true, // you should no longer use boolean false or true, now use false, true or 'new' learn more https://developer.chrome.com/articles/new-headless/
+      devtools: false, // Open devtools by default
+      debug: false, // Opens a debug session
+      logQR: true, // Logs QR automatically in terminal
+      browserWS: '', // If u want to use browserWSEndpoint
+      browserArgs: [''], // Original parameters  ---Parameters to be added into the chrome browser instance
+      addBrowserArgs: [''], // Add broserArgs without overwriting the project's original
+      puppeteerOptions: {}, // Will be passed to puppeteer.launch
+      disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+      disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+      updatesLog: true, // Logs info updates automatically in terminal
+      autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+      createPathFileToken: false, // creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+      addProxy: [''], // Add proxy server exemple : [e1.p.webshare.io:01, e1.p.webshare.io:01]
+      userProxy: '', // Proxy login username
+      userPass: '' // Proxy password
+    },
+
+    // BrowserInstance
+    (browser, waPage) => {
+      console.log('Browser PID:', browser.process().pid);
+      waPage.screenshot({ path: 'screenshot.png' });
+    }
+  )
+  .then((client) => {
+    start(client);
+  })
   .catch((erro) => {
     console.log(erro);
   });
-
-function start(client) {
-  client.onMessage((message) => {
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Welcome Venom 🕷')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
-    }
-  });
-}
