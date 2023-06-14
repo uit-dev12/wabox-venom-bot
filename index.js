9b4586ac5556155cc4fe0144cca3091779a7550a
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2023-06-14 20:50:47 +0700

    Wabox Broadcasting

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const { createDevices } = require('./src/controller/device')
+const { client_data } = require('./utils/waConnections/connections')
 const app = express()
 const port = 3000 
+const bodyParser = require('body-parser')
+const { messageRoutes } = require('./routes/messageRoutes')
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => { 
-	res.json.status(400).json({"name": "aditiya"})
-})
+app.use('/api/v1/message', messageRoutes)
 
 app.listen(port, () => {
 	console.log(`Server running in port ${port}`)
