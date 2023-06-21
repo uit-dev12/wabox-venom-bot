eafcc4606683db55908e429c9bc6c4a799a96345
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2023-06-21 20:59:00 +0700

    fix bug

--- utils/waConnections/connections.js
+++ utils/waConnections/connections.js
@@ -3,44 +3,52 @@ const { UpdateDevices } = require('../callApi')
 const client_data = []
 
 async function start(client, tokens, data, io) {
-  // push array clinet data 
-  client_data.push({client: client, tokens: tokens})
-  // socket send 
-  io.emit('status-'+tokens, {code: 200});
 
-  // get phone number and check phone number 
-  const getDeveices = await client.getHostDevice()
-  const phoneNumbers = getDeveices.id.user
-  console.log(phoneNumbers);
-
-  // check numbers phone logout if != number phone 
-  if(phoneNumbers != data.number_phone) { 
-      await client.logout();
-  } 
-
-// check connections and update devices is disconnect 
-const disconnect   = await  client.isConnected()
-if(!disconnect) { 
-  await UpdateDevices(tokens, 0)
-}
-// update devices connected 
-  await UpdateDevices(tokens, 1)
-
-
-
-  // replaying message 
-  client.onMessage((message) => {
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Welcome Venom 🕷')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
+  try { 
+      // push array clinet data 
+      client_data.push({client: client, tokens: tokens})
+      // socket send 
+      io.emit('status-'+tokens, {code: 200});
+    
+      // get phone number and check phone number 
+      const getDeveices = await client.getHostDevice()
+      const phoneNumbers = getDeveices.id.user
+      console.log(phoneNumbers);
+    
+      // check numbers phone logout if != number phone 
+      if(phoneNumbers != data.number_phone) { 
+          await client.logout();
+      } 
+    
+    // check connections and update devices is disconnect 
+    const disconnect   = await  client.isConnected()
+    if(!disconnect) { 
+      await UpdateDevices(tokens, 0)
     }
-  });
+    // update devices connected 
+      await UpdateDevices(tokens, 1)
+      // replaying message 
+      client.onMessage((message) => {
+        if (message.body === 'Hi' && message.isGroupMsg === false) {
+          client
+            .sendText(message.from, 'Welcome Venom 🕷')
+            .then((result) => {
+              console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+              console.error('Error when sending: ', erro); //return object error
+            });
+        }
+      });
+
+  } catch(e) { 
+    // erros 
+    io.emit('status-'+tokens, {code: 500});
+    UpdateDevices(tokens, 0)
+    console.log(e)
+
+
+  }
 }
 
 
